ea0e171779f2451e095d7a455d7a7d0774fd3e2b
Capten Sepp <sepp.hashemi.services@gmail.com>
2026-07-20 16:52:27 +0200

    feat: unify Shopella branding and design tokens

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
-    <main className="flex-1"> {/* route outlet wrapper */}
+    <main id="main-content" className="flex-1" tabIndex={-1}> {/* route outlet wrapper */}
       <Outlet /> {/* nested route content */}
     </main>
   );
